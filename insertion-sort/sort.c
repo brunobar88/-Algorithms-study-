b170bce5f90a5206da1fdbd41df5645ac3b46a65
Bruno B. Zanella <brunobzanella@gmail.com>
2021-04-24 21:35:13 -0300

    Add Explanatory comments

--- insertion-sort/sort.c
+++ insertion-sort/sort.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
 
-int main() {
-  int numbers[6] = {65, 89, 12, 45, 2, 90};
+void printNumberSet(int numbers[6]) {
+  for (int index = 0; index < 6; index++) { 
+    printf("%d ", numbers[index]);
+  }
 
+  printf("\n");
+}
+
+int main() {
+  int numbers[6] = {65, 89, 12, 45, 2, 90}; // Declaração do conjunto a ser ordenado.
+
+  // Primeiro loop que percorre cada elemento do array a partir do segundo.
+  // Da direita para a esquerda
   for (int j = 1; j < 6; j++) {
-    int key = numbers[j]; 
+    int key = numbers[j]; // Declaração da key(chave).
 
     int i = j - 1;
 
-    while (i >= 0 && numbers[i] > key) {
-      numbers[i + 1] = numbers[i];
+    // Segundo loop da esquerda para a direita a partir da posição da chave.
+    while (i >= 0 && numbers[i] > key) { 
+      numbers[i + 1] = numbers[i]; // Permutação de elmentos
 
       i = i - 1;
     }
 
-    numbers[i + 1] = key;
+    numbers[i + 1] = key; // Permutação de elmentos
   }
 
-  for (int index = 0; index < 6; index++) { 
-    printf("%d ", numbers[index]);
-  }
-  printf("\n");
+  // Função para imprimir o conjunto ordenado
+  printNumberSet(numbers);  
 
   return 0;
 }
